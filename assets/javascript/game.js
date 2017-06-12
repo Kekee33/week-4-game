0c9a4a94612d74afcb5c18e8d8f59d88a965732f
Keisha Leger <Keisha Leger>
2017-06-12 09:56:39 -0400

    added the basics

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// //computer will pick a random number
+// loop through math.random to find random number
+
+
+//numbers assigned to crystals
+
+// //user clicks a crystal
+// event listener needed 
+// $('button').on something 
+
+// //computer will add numbers 
+// review calculator  
+
+	
+// //if user reaches random numer, alert you win and reset
+
+
+
+// //if user surpasses random number, alert you lose
+
+
+//wins and losses should show in appropriate boxes 
