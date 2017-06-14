073df309b608db1543796e02ac93828e40304284
Keisha Leger <Keisha Leger>
2017-06-14 11:39:16 -0400

    working on displaying random num to screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,78 @@
-(document).ready(function)()
+$(document).ready(function() {
+});
 	
-	$(".btn") .on ("click", function() {
-		alert("Let's Play!");
-})
+	
+	//$(".btn") .on ("click", function() {
+		//alert("Let's Play!");
+//})
+  targetNum = Math.floor(Math.random()* 100);
+  targetNum[Math.floor(Math.random() * 100)];
+  $("#targetScore").html(targetNum);
+  console.log("target score is " + targetNum);
 
-var targetNum = 75
-var randomNum = math.floor(math.random() * 100
-var counter = 0
+//  var counter = 0;
 
-// //computer will pick a random number
+// var numberOptions = [5, 10, 20];
+//   var increment = numberOptions[Math.round(Math.random())];
+
+
+//   $(".shells").on("click", function() {
+//      counter += increment;
+
+//      alert("Current score: " + counter);
+
+//   });
+// //numbers need to show in score box
+ $("#targetScore").text(targetNum); 
+
+// for (var i = 0; i < numberOptions.length; i++) {
+// 	 var shellImage = $("<img>");
+// 	 shellImage.addClass("shell-image");
+
+// 	shellImage.attr("http://clipartix.com/wp-content/uploads/2016/06/Transparent-seashell-clipart.png");
+
+// 	shellImage.attr("data-shellvalue", numberOptions[i]);
+
+// 	$(".crystals").append(imageCrystal);
+
+// 	$(".shell-image").on("click", function() {
+
+// var shellValue = ($(this).attr("data-shellvalue"));
+//     shellValue = parseInt(shellValue);		
+
+// // //if user reaches random numer, alert you win and reset
+// counter += shellValue;
+
+// if (counter === targetNum) {
+// 	 alert("You win!");
+//     }
+ 
+//  	$("#clear").on("click", function() {
+//        $("#display").empty();
+//     });
+
+// if (counter >= targetNum) {
+
+//       //Alert with a loss.
+//       alert("You lose!!");
+//     }
+// });
+
+
+
+//computer will pick a random number
 // use math.random to find random number
 
-var random = targetNum[math.floor(math.random() * 100)];
-console.log(randomNum);
+// console.log(randomNum);
 
-//number displays to screen
-$(document).on("click", random);
+// //number displays to screen
+// $(document).on("click", random);
 
 
 
 //numbers assigned to crystals
 
-// //user clicks a crystal
-// event listener needed 
-// $('button').on something 
-
-// //computer will add numbers 
-// review calculator  
-
-	
-// //if user reaches random numer, alert you win and reset
-
-// $("#clear").on("click", function() {
-//       $("#display").empty();
-//             });
+// //user clicks a crystal 
 
 
 // //if user surpasses random number, alert you lose
